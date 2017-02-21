a12b1436f83fe192385d3c69f37cdab958defb76
Golo Roden <golo.roden@thenativeweb.io>
2017-02-21 20:35:45 +0100

    Update RabbitMQ in tests to 3.6.6-alpine.

--- test/integration/pre.js
+++ test/integration/pre.js
@@ -5,7 +5,7 @@ const shell = require('shelljs');
 const waitForRabbitMq = require('../helpers/waitForRabbitMq');
 
 const pre = function (done) {
-  shell.exec('docker run -d -p 5673:5672 --name rabbitmq rabbitmq:3.4.4', errExec => {
+  shell.exec('docker run -d -p 5673:5672 --name rabbitmq rabbitmq:3.6.6-alpine', errExec => {
     if (errExec) {
       return done(errExec);
     }
